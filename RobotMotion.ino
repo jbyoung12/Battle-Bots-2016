f6bb1152684d200ec7a041a3613f3cb834cae398
Justin Wei <justin.lj.wei@gmail.com>
2016-09-24 16:22:19 -0400

    added functionality to RobotMotion

--- RobotMotion.ino
+++ RobotMotion.ino
@@ -1,20 +1,38 @@
 #include <AFMotor.h>
 
-// Objects
+// Objects (two motors)
 AF_DCMotor leftMotor(1, MOTOR12_64KHZ);
 AF_DCMotor rightMotor(2, MOTOR12_64KHZ);
 const int motorMaxSpeed = 255;
 
-// Setup
-void motorSetup() {
-    leftMotor.setSpeed(motorMaxSpeed*.8);
-    rightMotor.setSpeed(motorMaxSpeed*.8);
+// Set up initial speed of motors
+void initialMotorSetup() {
+    setSpeedMotors(0.8,0.8);
 }
 
 
-// Running motors
-void runMotors() {
-    leftMotor.run(FORWARD);
-    rightMotor.run(FORWARD);
+// ----------------------- MOTOR CONTROLS ----------------------- //
+
+// moves robot forward for time t (in seconds)
+void forwardMotorsForTime(int time) {
+  forwardMotors();
+  delay(time*1000);
+  stopMotors();
+}
+
+// runs both motors forward infinitely
+void forwardMotors() {
+  leftMotor.run(FORWARD);
+  rightMotor.run(FORWARD);
+}
+// stops both motors
+void stopMotors() {
+  leftMotor.run(RELEASE);
+  rightMotor.run(RELEASE);
+}
+// sets speed of motors (input should be decimal referring to % of maxSpeed)
+void setSpeedMotors(int m1Speed, int m2Speed) {
+    leftMotor.setSpeed(motorMaxSpeed * m1Speed);
+    rightMotor.setSpeed(motorMaxSpeed * m2Speed);
 }
 
