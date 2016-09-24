22fce549e4c28ba7df9d1bd1216a3c5ac1acd71b
Justin Wei <justin.lj.wei@gmail.com>
2016-09-24 15:47:52 -0400

    Left and right motors run infinitely forward and HelperMethods file added

--- RobotMotion.ino
+++ RobotMotion.ino
@@ -1,1 +1,20 @@
+#include <AFMotor.h>
+
+// Objects
+AF_DCMotor leftMotor(1, MOTOR12_64KHZ);
+AF_DCMotor rightMotor(2, MOTOR12_64KHZ);
+const int motorMaxSpeed = 255;
+
+// Setup
+void motorSetup() {
+    leftMotor.setSpeed(motorMaxSpeed*.8);
+    rightMotor.setSpeed(motorMaxSpeed*.8);
+}
+
+
+// Running motors
+void runMotors() {
+    leftMotor.run(FORWARD);
+    rightMotor.run(FORWARD);
+}
 
