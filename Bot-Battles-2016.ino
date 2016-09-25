ad5eac776b5299eaadf0e940d507b14a92365dc4
Andrew Arnold <andrewarnold@Andrews-MacBook-Pro.local>
2016-09-25 14:44:10 -0400

    addded setup and cycle functions

--- Bot-Battles-2016.ino
+++ Bot-Battles-2016.ino
@@ -4,9 +4,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   initialMotorSetup();
+  setupSonar();
+  setupCompass();
 }
 
 void loop() {
-  forwardRobotForTime(5);
-  stopRobotForTime(2);
+  
+  //  forwardRobotForTime(5);
+  //  stopRobotForTime(2);
+  forwardBothMotors();
+  sonarCyle();
+
+  //compassCycle();  
 }
